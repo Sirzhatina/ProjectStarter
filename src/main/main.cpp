533d7cec7eda5b94c6f123749dd90454c9c451c9
Sirzhatina <mazurkevichsergey@gmail.com>
2023-12-18 20:01:30 +0300

    screwed my own implementation attempt and switched to external deps

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,20 +1,5 @@
-import proj_maker;
-import utility;
 
 auto main(int argc, char* argv[]) -> int
 {
-    
-
-    if (argc < 3)
-    {
-        return util::exitMainWithFailure("The call must have at least two arguments provided, "
-                                   "e.g. \"teframe project_name cmake_version\"");
-    }
 
 }
-
-/* 
-Order of the actions:
-Take all arguments -> parse arguments -> set corresponding values -> generate project structure
-
- */
